e3ef6258f6328353b0b160013214a5234d1ff0c9
Zealon <zealon@126.com>
2018-10-16 15:05:25 +0800

    Initial commit

---
+++ src/cn/zealon/array/TwoSum.java
@@ -0,0 +1,42 @@
+package cn.zealon.array;
+
+
+/**
+ * 两数之和
+ *
+ * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+ *
+ * 示例:
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ * @auther: Zealon
+ * @Date: 2018-10-16 14:50
+ *
+ */
+public class TwoSum {
+
+    public static void main(String[] args){
+        int[] nums = {1,0,5,3,1,2,45,10};
+        int[] resultNums = twoSum(nums,11);
+        if(resultNums!=null){
+            System.out.println(resultNums[0]+","+resultNums[1]);
+        }
+    }
+
+    /**
+     * 暴力检查法
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j]==target - nums[i]){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
