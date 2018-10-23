2038b84dec310f0b773ef25dcce28954c6698d75
Zealon <zealon@126.com>
2018-10-23 15:13:07 +0800

    提交 二分查找法.

---
+++ src/main/java/cn/zealon/array/BinarySearch.java
@@ -0,0 +1,42 @@
+package cn.zealon.array;
+
+/**
+ * 二分查找算法
+ * @auther: Zealon
+ * @Date: 2018-10-23 11:28
+ */
+public class BinarySearch {
+
+    public static void main(String[] args){
+
+        int [] array = {1,2,3,4,5,6,7,12,19,55};
+        for(int i=0;i<array.length;i++){
+            //System.out.println(binarySearch(array,array[i]));
+        }
+        System.out.println(binarySearch(array,6));
+
+    }
+
+    /**
+     * 二分查找
+     * @param nums 递增数组
+     * @param target 待查找数值
+     * @return
+     */
+    private static int binarySearch(int[] nums,int target) {
+        if(null == nums) return -1;
+        int low = 0,high = nums.length-1;
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+            if(target == nums[mid]){
+                return mid;
+            }else if(target < nums[mid]){
+                high = mid - 1;
+            }else if(target > nums[mid]){
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+}
---
+++ src/main/java/object/CaseObject.java
@@ -0,0 +1,16 @@
+package object;
+
+/**
+ * Object 常用方法
+ * @auther: Zealon
+ * @Date: 2018-10-23 10:24
+ */
+public class CaseObject {
+    public static void main(String[] args){
+        Object obj = new Object();
+        obj.toString();
+        obj.equals("");
+        obj.hashCode();
+        obj.getClass();
+    }
+}
