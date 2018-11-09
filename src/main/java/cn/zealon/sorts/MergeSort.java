01910e431bcf5db6ef9f1b73ca17f257879acc34
Zealon <zealon@126.com>
2018-11-09 17:09:37 +0800

    快速排序.

--- src/main/java/cn/zealon/sorts/MergeSort.java
+++ src/main/java/cn/zealon/sorts/MergeSort.java
@@ -1,5 +1,6 @@
 package cn.zealon.sorts;
 
+
 import java.util.Arrays;
 
 /**
@@ -9,10 +10,70 @@ import java.util.Arrays;
  */
 public class MergeSort {
     public static void main(String[] args){
-        int[] arr = {2,1,61,12,6,10};
-        arr = mergeSort(arr);
+        int[] arr = {2,1,61,12,6,10,3};
+        arr = mergeSort1(arr);
         System.out.println(Arrays.toString(arr));
+
+        System.out.println(5 << 2);
+/*        int[] left = {1,3};
+        int[] right = {2,4};
+        System.out.println(left.length);
+        int[] nums = merge1(left,right);
+
+        System.out.println(Arrays.toString(nums));*/
+
     }
+
+    public static int[] mergeSort1(int[] nums){
+        if(nums.length<2){
+            return nums;
+        }
+        int mid = nums.length/2;
+        int[] left = Arrays.copyOfRange(nums,0,mid);
+        int[] right = Arrays.copyOfRange(nums,mid,nums.length);
+        return merge1(mergeSort1(left),mergeSort1(right));
+    }
+
+    // 合并已经排好序的2个数组
+    public static int[] merge1(int[] left,int[] right){
+        int[] result = new int[left.length+right.length];
+        for (int index = 0,i=0,j=0; index < result.length; index++) {
+            if(i>=left.length){
+                result[index]=right[j++];
+            }else if(j>=right.length){
+                result[index]=left[i++];
+            }else if(left[i]>right[j]){
+                result[index]=right[j++];
+            }else if(left[i]<right[j]){
+                result[index]=left[i++];
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public static int[] mergeSort(int[] arr){
         if(arr.length<2){
@@ -42,4 +103,38 @@ public class MergeSort {
         }
         return result;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/cn/zealon/sorts/QuickSort.java
+++ src/main/java/cn/zealon/sorts/QuickSort.java
@@ -1,8 +1,12 @@
 package cn.zealon.sorts;
 
+import cn.zealon.utils.Utils;
+
 import java.lang.ref.SoftReference;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 /**
  * 快速排序
@@ -11,11 +15,72 @@ import java.util.List;
  */
 public class QuickSort {
     public static void main(String[] args){
-        SoftReference s;
+
+        int[] nums = {1,87,24,17,7,10};
+
+        int[] sortNums = quickSort(nums,0,nums.length-1);
+
+        System.out.println(Arrays.toString(sortNums));
+
     }
 
-    public static void quickSort(){
-        List<String> list = new ArrayList<>();
-        Integer i = 5;
+    public static int[] quickSort(int[] arr,int start,int end){
+        int minIndex = partition(arr,start,end);
+        if (minIndex > start){
+            quickSort(arr,start,minIndex-1);
+        }
+        if (minIndex < end){
+            quickSort(arr,minIndex+1,end);
+        }
+        return arr;
     }
+
+
+    public static int partition(int[] arr,int start,int end){
+        // 获取基准点
+        int pivot = (int) (start+Math.random()*(end-start+1));
+
+        // 将基准点放到数组最后面（交换）
+        int temp = arr[pivot];
+        arr[pivot] = arr[end];
+        arr[end] = temp;
+
+        // 比基准数大的索引下标，用于交换位置
+        int minIndex = start - 1;
+
+        for (int i = start; i <= end; i++) {
+            // 依次对 i 元素，与基准数对比
+            if(arr[i] <= arr[end]){
+                minIndex++;
+                if(i>minIndex){
+                    int tmp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = tmp;
+                }
+            }
+        }
+        return minIndex;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
